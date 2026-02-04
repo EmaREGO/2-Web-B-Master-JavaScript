875e966610c2b108575c6b9caec07b685b183198
José Emanuel RG <resendizgonzalezjoseemanuel@gmail.com>
2026-02-03 21:49:55 -0600

    Add: Problema resuelto en archivo while.js

--- ciclos.js
+++ ciclos.js
@@ -13,9 +13,9 @@ while(entrada !== 'salir'){
     entrada = prompt('Escribe algo')
 } */
 /* 
-console.log('Yeih, saliste'); */
+/* console.log('Yeih, saliste'); */
 
-let frutas = ['Manzana', 'Pera', 'Lulo', 'Cereza', 'Mango', 'Papaya'];
+/* let frutas = ['Manzana', 'Pera', 'Lulo', 'Cereza', 'Mango', 'Papaya'];
 
 console.log(frutas[3]);
 console.log(frutas[2]);
@@ -26,3 +26,86 @@ console.log(frutas.length);
 for (let indice = 0; indice < frutas.length; indice++){
     console.log("Fruta = ", frutas[indice])
 }
+
+console.log('Metodos de los arreglos')
+let alumnos = []; */
+
+//Metodo push agrega al final
+/* alumnos.push("Ana");
+alumnos.push("Bruno");
+alumnos.push("Fernanda");
+alumnos.push("Natalia");
+
+console.log(alumnos);
+
+//Elimina quita el ultimo
+alumnos.pop();
+
+console.log(alumnos);
+ */
+//Metodo unshift agrega al principio
+/* alumnos.unshift("Nina");
+alumnos.unshift("Misael");
+alumnos.unshift("Beto");
+
+console.log(alumnos);
+
+//Metodo shift quita al principio
+alumnos.shift();
+console.log(alumnos);
+ */
+//Arreglo de una lista de super 
+//Imprimir la lista del super
+/* 
+1. Manzana
+2. Papas
+3. Jabon
+4. Papel de banio
+
+total de prductos
+prompt GUARDAR LA LITA QUE TE DA EL USUARIO DENTRO DE UN ARREGLO\
+ */
+
+
+let carrito = [];
+let indice = 0;
+
+carrito.push("Manzana");
+carrito.push("Papas");
+carrito.push("Jabon");
+carrito.push("Jabon"); 
+
+console.log(carrito);
+
+console.log("1.", carrito[0]);
+console.log("2.", carrito[1]);
+console.log("3.", carrito[2]);
+console.log("4.", carrito[3]);
+
+console.log("Total de productos:", carrito.length);
+
+let listaDeSuper = ['Manzana', 'Servilletas', 'Jabon', 'Aguacate', 'Tortillas'];
+
+for (let i = 0; i < listaDeSuper.length; i++){
+    console.log((i+1) + " - " + listaDeSuper[i]);
+}
+console.log("Total de productos:", listaDeSuper.length)
+
+
+let listaSuper = [];
+let producto = "Inicio";
+
+while (producto !== "") {
+    producto = prompt('Ingresa un producto.')
+
+    if (producto !== ""){
+        listaSuper.push(producto);
+    }
+}
+
+for (let indice = 0; indice < listaSuper.length; indice++){
+    console.log((indice+1) + ". " + listaSuper[indice]);
+}
+console.log("Total de la lista: ",listaSuper.length);
+
+
--- while.js
+++ while.js
@@ -1,86 +1,27 @@
-let contador = 10;
+/* 
+Problema: Clasificación de Frutas
+Imagina que tienes un programa que clasifica las frutas según su tipo y cuenta cuántas hay de cada tipo.
 
-while ( contador > 0) {
-    console.log(contador);
-    contador --;
-};
+Instrucciones para resolver el problema:
+Declara un arreglo llamado frutas con varios tipos de frutas.
+Crea un objeto para almacenar la cantidad de cada tipo de fruta.
+Usa un ciclo for/while para recorrer el arreglo y contar las frutas.
+Imprime en la consola la cantidad de cada tipo de fruta.
+Opcional: intenta implementar la solución con el otro ciclo también (for/while).
+*/
 
-console.log("______________");
+let listaSuper = [];
+let producto = "Inicio";
 
-for (let i = 0; i < 10; i++ ){
-    console.log(i);
-};
+while (producto !== "") {
+    producto = prompt('Ingresa un producto.')
 
-console.log("______________");
-let suma = 0;
-
-for (let i = 1; i <= 10; i++) {
-    suma += i; 
-
+    if (producto !== ""){
+        listaSuper.push(producto);
+    }
 }
-console.log(suma); 
 
-console.log("___________");
-
-let frutas = ['manzana', 'naranja', 'plátano', 'kiwi'];
-
-console.log(frutas[1]);
-
-frutas[2] = 'Coco'; 
-
-console.log(frutas);
-
-//Puedes acceder a los elementos de un array directamente mediante su índice:
-
-console.log(frutas[0]); // Salida: "manzana"
-
-console.log(frutas[2]); // Salida: "plátano"
-
-//Puedes modificar un elemento de un array asignando un nuevo valor a un índice específico:
-
-frutas[1] = 'pera'; 
-
-// Ahora el array es ['manzana', 'pera', 'plátano', 'kiwi']
-//Hay varias formas de añadir elementos a un array. La más común es usar el método push(), que añade un elemento al final del array:
-
-frutas.push('kiwi');
-
-console.log(frutas); 
-
-// Imprime: ['manzana', 'pera', 'naranja', 'kiwi']
-//También puedes usar unshift() para añadir un elemento al principio:
-
-frutas.unshift('cereza');
-
-console.log(frutas); 
-
-// Imprime: ['cereza', 'manzana', 'pera', 'naranja', 'kiwi']
-//Para eliminar un elemento del final del array, utilizamos pop():
-
-frutas.pop();
-
-console.log(frutas); 
-
-// Imprime: ['cereza', 'manzana', 'pera', 'naranja']
-// Si quieres eliminar el primer elemento, puedes usar shift():
-
-frutas.shift();
-
-console.log(frutas); 
-
-// Imprime: ['manzana', 'pera', 'naranja']
-//La propiedad length de un array devuelve la cantidad total de elementos en él, lo cual es útil para determinar el último índice válido:
-
-console.log(frutas.length); // Salida: 3
-
-console.log(frutas[frutas.length - 1]); // Salida: "'naranja'"
-//Esto es útil para iterar a través de los elementos del array.
-
-//Puedes usar un bucle for para recorrer un array:
-
-for (let i = 0; i < frutas.length; i++) {
-
-    console.log(frutas[i]); // Imprime cada fruta
-
+for (let indice = 0; indice < listaSuper.length; indice++){
+    console.log((indice+1) + ". " + listaSuper[indice]);
 }
-//
+console.log("Total de la lista: ",listaSuper.length);
