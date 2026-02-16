57491cb0356ae72f23b82e2158f155da00ace010
José Emanuel RG <resendizgonzalezjoseemanuel@gmail.com>
2026-02-16 16:58:18 -0600

    Add: Creación comentarios.html y comentario.js para resolución del problema de caja de comentarios

--- comentario.js
+++ comentario.js
@@ -1,11 +1,36 @@
+/* 
+Problema: Caja de Comentarios (Simple Comment Box)
+Crear una caja de comentarios donde los usuarios puedan 
+escribir y agregar comentarios que se muestran en la página. 
+Este proyecto ayudará a los estudiantes a manejar eventos de formularios 
+y modificar el contenido de una página.
+*/
 const formulario = document.getElementById("formulario");
 const input = document.getElementById("iComentario");
 const cajaComentario = document.getElementById("cajaComentario");
 
-
-
-formulario.addEventListener("click", (e) => {
-    e.preventDefault;
+formulario.addEventListener("submit", (e) => {
     const texto = input.value;
-    const comentario = document.createElement("p");
+    const fecha = new Date().toLocaleString()
+
+    e.preventDefault();
+    
+
+    let nuevoComentario = document.createElement('p'); //Lógica para agregar nuevo comentario
+    let btnBorrar = document.createElement('button');
+    btnBorrar.textContent = ('Eliminar');
+    nuevoComentario.textContent = texto + " " +fecha; //Agregar date comentario
+    cajaComentario.appendChild(nuevoComentario);
+    nuevoComentario.appendChild(btnBorrar);
+    input.value = ("");
+
+    btnBorrar.addEventListener("click", (e) => { //Lógica para eliminar comentario 
+        nuevoComentario.remove();
+        e.preventDefault();
+    });
+    
 });
+
+
+
+
